43bfd68a5f5519bdc17a7e4c047dc914845369c8
crystalcortez <crystal.cortez@pcc.edu>
2019-01-30 15:00:43 -0800

    firstcommit

---
+++ steepFast.js
@@ -0,0 +1,76 @@
+function setup() {
+	createCanvas(2000, 2000,);
+	fill(255);
+
+
+	
+
+}
+
+function draw() {
+	background(20);
+	
+	
+noStroke(0);
+fill(204);
+push();
+translate(windowWidth*.50, windowHeight*.999);
+
+
+fill(55);
+triangle(-100, -700, 50, 360, 81, 360);
+fill(50);
+triangle(-100, -500, 50, 360, 81, 360);
+fill(45);
+triangle(-100, -300, 50, 360, 81, 360);
+fill(40);
+triangle(-100, -200, 50, 360, 81, 360);
+fill(35);
+triangle(-100, -100, 50, 360, 81, 360);
+fill(30);
+ triangle(-100, -25, 50, 360, 81, 360);
+fill(25);
+  triangle(-100, 50, 50, 360, 81, 360);
+fill(20);
+triangle(-100, 150, 50, 360, 81, 360);
+fill(15);
+triangle(-100, 200, 50, 360, 81, 360);
+fill(10);
+triangle(-100, 250, 50, 360, 81, 360);
+fill(5);
+triangle(-100, 300, 50, 360, 81, 360);
+fill(0);
+triangle(-100, 350, 50, 360, 81, 360);
+pop();
+
+translate(windowWidth*.50, windowHeight*.999);
+noStroke();
+fill(0);
+triangle(200, -700, 50, 360, 81, 360);
+fill(5);
+triangle(200, -500, 50, 360, 81, 360);
+fill(10);
+triangle(200, -300, 50, 360, 81, 360);
+fill(15);
+triangle(200, -200, 50, 360, 81, 360);
+fill(20);
+triangle(200, -100, 50, 360, 81, 360);
+fill(25);
+triangle(200, -25, 50, 360, 81, 360);
+fill(30);
+  triangle(200, 50, 50, 360, 81, 360);
+fill(35);
+triangle(200, 150, 50, 360, 81, 360);
+fill(40);
+triangle(200, 200, 50, 360, 81, 360);
+fill(45);
+triangle(200, 250, 50, 360, 81, 360);
+fill(50);
+triangle(200, 300, 50, 360, 81, 360);
+fill(55);
+triangle(200, 350, 50, 360, 81, 360);
+  //line(50, 100, 70, 50);
+  //line(100, 50, 70, 50);
+  line(p2, p2, p3, p2);
+  line(p3, p2, p3, p3);
+}
